a41864d4c80d4967adb2500bcfdec38425e24299
StarThinking <sixiangma0408@foxmail.com>
2014-03-13 10:02:33 +0800

    commit before changing to ceph 0.72

--- TEST/new-hdd-ssd-test/skew/ssd-skew-4k-seq-write-new1/analyze.sh
+++ TEST/new-hdd-ssd-test/skew/ssd-skew-4k-seq-write-new1/analyze.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 counter=3 
+bs=1
+power=2
 
 for skew in 1.25 1.5 2.0 2.25 2.5 2.75 3.0 3.25 3.5 3.75 4.0
 do
@@ -11,5 +13,7 @@ do
 		sum=`expr $sum + $iops`
 	done
 	avg=`expr $sum / $counter`
-	echo "$skew avg iops = $avg"
+	bw=`expr $avg \* $bs`
+	bs=`expr $bs \* $power`
+	echo "$skew avg iops = $avg, bw = $bw KB"
 done
