df154de6e5ef1b2df91df1069823d84198959713
a45154630 <a45154630@163.com>
2015-04-22 17:14:27 +0800

    finish client resource usage experiment

--- experiments/MOBBS-info/parse.sh
+++ experiments/MOBBS-info/parse.sh
@@ -1,6 +1,6 @@
 parse_network()
 {
-	cat network.log | grep '^ *[0-9]' | awk '{i+=$1;o=$2;c+=1} END {ri=i/c;ro=o/c;print "net in",ri,"KB/S","net out",ro,"KB/S"}'
+	cat network.log | grep '^ *[0-9]' | awk '{i+=$1;o=$2;c++} END {ri=i/c;ro=o/c;print "net in",ri,"KB/S","net out",ro,"KB/S"}'
 }
 
 parse_cpu_mem()
