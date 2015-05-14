9738bd929e0ccf72d7d328fce1288171b7a3df6e
a45154630 <a45154630@163.com>
2015-05-14 12:03:17 +0800

    add storage1 experiments

---
+++ experiments/WHOBBS-info/storage1/mon.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ifstat > network.log &
+
+count=0
+while [ $count -lt 59 ]
+do
+#	ps -e -o 'pid,comm,pcpu,rsz' | grep qemu >> cpu_mem.log
+	sleep 5s
+	count=`expr ${count} + 1`
+done
+
+kill %1
